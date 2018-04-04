8b3886732f89b5cd410e3c42b89fb5173bdaf640
Caleb Seay <s179067@students.aie.edu.au>
2018-04-04 11:00:50 -0700

    work on turret defense

---
+++ Assets/Bullet.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public int damage;
+
+    public float lifetime;
+    public Vector3 dir;
+    float timer;
+    // Use this for initialization
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //timer += Time.deltaTime;
+
+
+        Debug.DrawLine(transform.position, transform.position + (dir * 10));
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "Enemy")
+        {
+            other.GetComponent<Moving>().currentHealth -= damage;
+            Destroy(gameObject);
+        }
+
+    }
+
+
+}
---
+++ Assets/Moving.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Moving : MonoBehaviour //idamageable
+{
+
+    public NavMeshAgent nav;
+    public GameObject target;
+    public int currentHealth;
+    public int Maxhealth;
+    float distance;
+    // Use this for initialization
+    void Start()
+    {
+        currentHealth = Maxhealth;
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    public bool editor;
+    void Update()
+    {
+        distance = Vector3.Distance(transform.position, target.transform.position);
+        if (editor)
+        {
+            editor = false;
+            takedamage(2);
+        }
+        nav.destination = target.transform.position;
+        die();
+    }
+    public void takedamage(int damagetaken)
+    {
+        currentHealth -= damagetaken;
+        die();
+    }
+    public void die()
+    {
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+        else if(distance <= 10)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //void OnCollisionEnter(Collision other)
+    //{
+    //    idamageable damage = other.gameObject.GetComponent<idamageable>();
+    //    if (damage != null)
+    //    {
+    //        damage.takedamage(attackDamage);
+    //    }
+    //}
+}
